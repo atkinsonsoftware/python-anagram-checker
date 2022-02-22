604d256044a85f37df4d653d0f1206892a740f28
Anna Atkinson <atkinsonsoftware@gmail.com>
2022-02-21 20:51:47 -0800

    Program to determine prime factors of a given integer.

---
+++ prime_factors.py
@@ -0,0 +1,27 @@
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("value", type=int)
+
+    args = parser.parse_args()
+    value = args.value
+
+    print(value)
+    factors = []
+    for i in range(2, value):
+        if (value % i == 0):
+            print("Testing i: %d" % i)
+            if len(factors) > 1:
+                for f in factors[:]:
+                    if (i % f == 0):
+                        print(" not adding i %d" % i)
+                        print(" because factor f %d already counted" % f)
+                        break
+                    else:
+                        factors.append(i)
+                        break
+            else:
+                factors.append(i)
+
+    print("%d's factors are: %s" % (value, factors))
