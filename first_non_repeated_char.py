e0c7464cec42dfab4d31af0f2139bdea4d6fe672
Anna Atkinson <atkinsonsoftware@gmail.com>
2022-02-21 20:32:35 -0800

    Add kth non repeated char function

--- first_non_repeated_char.py
+++ first_non_repeated_char.py
@@ -6,6 +6,17 @@ def findFirstNonRepeatedChar(word):
     else:
         return None
 
+def findKthNonRepeatedChar(word, k):
+    word = word.lower()
+    counter = 0
+    for i in word:
+        if (word.count(i) == 1):
+            counter += 1
+            if (counter == k):
+                return i
+    else:
+        return None
+
 if __name__ == "__main__":
     word_list = ["scope",
                  "stellar",
@@ -26,4 +37,13 @@ if __name__ == "__main__":
                   (word, unique_char))
         else:
             print("Word '%s' only contains repeating characters." % (word, ))
-                                                                     
+
+    k_for_problem = 2
+    for word in word_list:
+        unique_char = findKthNonRepeatedChar(word, k_for_problem)
+        if unique_char:
+            print("Word '%s's %d non-repeating character is '%s'." %
+                  (word, k_for_problem, unique_char))
+        else:
+            print("Word '%s' does not have %d non-repeating characters." %
+                  (word, k_for_problem))
