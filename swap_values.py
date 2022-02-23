b0b7976f05b121d7b5c57b45858cb645020f22fb
Anna Atkinson <atkinsonsoftware@gmail.com>
2022-02-23 14:41:20 -0800

    Swap values for a string input

--- swap_values.py
+++ swap_values.py
@@ -27,3 +27,17 @@ if __name__ == "__main__":
     second = args.second
     print("About to use function. first: %d second %d" % (first, second))
     print("Now: first: %d second %d" % swapValues(first, second))
+
+    # swap strings
+    str1 = "first_string"
+    str2 = "second_string"
+    print(str1)
+    print(str2)
+    # Concatenate, place in second var
+    str2 += str1
+    # Update first string by grabbing chars up to first strings length
+    str1 = str2[:(len(str1) + 1):]
+    # Update second string by grabbing chars starting at first strings length
+    str2 = str2[len(str1):]
+    print(str1)
+    print(str2)
